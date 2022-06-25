bf01cb2b329338382d2aff0f6c17cfda10b961cb
Luiz Rioja <lulyrioja@gmail.com>
2022-06-24 21:42:14 -0300

    chore(Lesson): add Link element

--- src/components/Lesson.tsx
+++ src/components/Lesson.tsx
@@ -1,6 +1,7 @@
 import { format, isPast } from "date-fns";
 import ptBR from "date-fns/locale/pt-BR";
 import { CheckCircle, Lock } from "phosphor-react";
+import { Link } from "react-router-dom";
 
 interface LessonProps {
   title: string;
@@ -18,10 +19,10 @@ export function Lesson({ title, slug, type, availableAt }: LessonProps) {
   );
 
   return (
-    <a href="#">
+    <Link to={`/event/lesson/${slug}`} className="group">
       <span className="text-gray-300">{availableDateFormat}</span>
 
-      <div className="rounded border border-gray-500 p-4 mt-2">
+      <div className="rounded border border-gray-500 p-4 mt-2 group-hover:border-green-500">
         <header className="flex items-center justify-between">
           {isLessonAvailable ? (
             <span className="flex items-center gap-2 text-sm text-blue-500 font-medium">
@@ -42,6 +43,6 @@ export function Lesson({ title, slug, type, availableAt }: LessonProps) {
 
         <strong className="text-gray-200 mt-5 block">{title}</strong>
       </div>
-    </a>
+    </Link>
   );
 }
