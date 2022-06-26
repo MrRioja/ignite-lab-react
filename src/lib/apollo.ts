45c3d473c0a3cced703ae27a7c6b401ec3cadf9c
Luiz Rioja <lulyrioja@gmail.com>
2022-06-26 19:36:34 -0300

    chore: change environment variables in apolloClient

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,6 +1,9 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-  uri: "https://api-sa-east-1.graphcms.com/v2/cl4oax18s1jba01xrf8p03b5v/master",
+  uri: import.meta.env.VITE_API_URL,
+  headers: {
+    Authorization: `Bearer ${import.meta.env.VITE_API_ACCESS_TOKEN}`,
+  },
   cache: new InMemoryCache(),
 });
